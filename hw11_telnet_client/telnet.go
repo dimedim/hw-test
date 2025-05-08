7f4ec645bb13a9a42d660a53500f29142cdeae82
dimedim <safemysave@gmail.com>
2025-05-08 21:38:24 +0300

    HW11 is completed

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"io"
+	"net"
+	"os"
 	"time"
 )
 
@@ -12,10 +16,50 @@ type TelnetClient interface {
 	Receive() error
 }
 
+type Telenet struct {
+	Address string
+	Timeout time.Duration
+	Conn    net.Conn
+	In      io.ReadCloser
+	Out     io.Writer
+}
+
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &Telenet{
+		Address: address,
+		Timeout: timeout,
+		In:      in,
+		Out:     out,
+	}
+}
+
+func (t *Telenet) Connect() error {
+	conn, err := net.DialTimeout("tcp", t.Address, t.Timeout)
+	if err != nil {
+		return err
+	}
+
+	t.Conn = conn
+	fmt.Fprintf(os.Stderr, "...Connected to %s\n", t.Address)
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (t *Telenet) Close() error {
+	return t.Conn.Close()
+}
+
+func (t *Telenet) Send() error {
+	_, err := io.Copy(t.Conn, t.In)
+	if errors.Is(err, io.EOF) {
+		return nil
+	}
+	return err
+}
+
+func (t *Telenet) Receive() error {
+	_, err := io.Copy(t.Out, t.Conn)
+	if errors.Is(err, io.EOF) || errors.Is(err, net.ErrClosed) {
+		return nil
+	}
+	return err
+}
